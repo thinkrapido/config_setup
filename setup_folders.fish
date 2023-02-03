bbe5e1bbdb08ba362caf818845ca727d5bee7036
Romeo Disca <romeo.disca@gmail.com>
2023-02-03 06:28:49 +0100

    initial commit

---
+++ setup_folders.fish
@@ -0,0 +1,19 @@
+#!/bin/fish
+
+set config_folder ~/.config
+set archive_folder ~/Mothership/config
+
+function setup
+
+    set folder (basename $argv[1])
+    echo setting up folder [$folder]
+
+	rm -rf $config_folder/$folder 2> /dev/null
+    ln -s $archive_folder/$folder $config_folder/$folder
+
+end
+
+for f in (find $archive_folder -maxdepth 1 -type d | sed '1 d')
+    setup $f
+end
+
