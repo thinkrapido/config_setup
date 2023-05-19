e3f452c3e84707ec5567b23ac3581c35722be54c
Romeo Disca <romeo.disca@gmail.com>
2023-05-19 23:04:48 +0200

    added treesitter variation for ubuntu and nixos nvim config

--- config/nvim/after/plugin/treesitter.lua
+++ config/nvim/after/plugin/treesitter.lua
@@ -1,6 +1,46 @@
+
+local function read_stdout(cmd)
+    local process = io.popen(cmd)
+    local contents = process:read("*a")
+    process:close()
+    return contents
+end
+
+local function script_path()
+    local str = debug.getinfo(2, "S").source:sub(2)
+    return str:match("(.*[/\\])") or "./"
+end
+
+local function filename()
+    return script_path() .. '/nix-info.txt'
+end
+
+local function docs()
+
+    local contents = ""
+    local f = io.open(filename(), "r")
+    if f then
+        contents = f:read("*a")
+        f:close()
+    else
+        contents = read_stdout("nix-shell -p nix-info --run 'nix-info -m'")
+        f = io.open(filename(), "w")
+        f:write(contents)
+        f:close()
+    end
+
+    local doc = "help"
+    if string.find(contents, ", NixOS,") then
+        doc = "vimdoc"
+    end
+
+    return doc
+
+end
+
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the four listed parsers should always be installed)
-  ensure_installed = { "typescript", "javascript", "rust", "haskell", "c", "lua", "vim", "help" },
+  ensure_installed = { "typescript", "javascript", "rust", "haskell", "c", "lua", "vim", docs() },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
