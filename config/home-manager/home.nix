a2a9449ec7de408782a947762f21093c86a4213b
Romeo Disca <romeo.disca@gmail.com>
2023-05-19 20:05:20 +0200

    added allowing unfree nix

--- config/home-manager/home.nix
+++ config/home-manager/home.nix
@@ -50,10 +50,6 @@
       obsidian
   ];
 
-  nixpkgs.overlays = [
-    (import ./firefox.overlay)
-  ];
-
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
