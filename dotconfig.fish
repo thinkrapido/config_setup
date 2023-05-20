1f2ec1326f480af36822baf29f4f16a2a7a8c50c
Romeo Disca <romeo.disca@gmail.com>
2023-05-21 01:27:08 +0200

    fixed dotconfig.fish

--- dotconfig.fish
+++ dotconfig.fish
@@ -20,6 +20,23 @@ function delete_broken_links
     find $argv[1] -xtype l -delete
 end
 
+function create_links
+
+    rm $archive_folder/*
+    set folders (rg dotconfigs config/dotconfig-folders.nix | sed 's$.*\.\./$$g')
+    rm -rf setup/dotconfig 2> /dev/null
+    mkdir -p setup/dotconfig
+    for f in $folders
+        for ff in (dir -1 "$f")
+            set file $f/$ff
+            ln -s ../../$file $archive_folder
+        end
+    end
+
+end
+
+create_links
+
 for f in (find $archive_folder -maxdepth 1 -type l | sed '1 d; /.git/ d')
     setup $f
 end
