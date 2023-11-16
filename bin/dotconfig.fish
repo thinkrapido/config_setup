8bdf556c1027061e363a84efbd94b673405cdd80
Romeo Disca <romeo.disca@gmail.com>
2023-11-16 12:06:02 +0100

    qemu stuff corrected

--- bin/dotconfig.fish
+++ bin/dotconfig.fish
@@ -1,7 +1,7 @@
 #!/usr/bin/env fish
 
 set config_folder  ~/.config/
-set archive_folder ~/Mothership/config/dotconfigs/combo-$argv[1]
+set archive_folder $config_folder/Mothership/config/dotconfigs/combo-$argv[1]
 
 find $config_folder -xtype l -delete
 for f in (find $archive_folder -maxdepth 1 -type l | sed '1 d; /.git/ d')
