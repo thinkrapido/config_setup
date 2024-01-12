f5c21987a0b31f25c33156d1493476ad4e9f3473
Romeo Disca <romeo.disca@gmail.com>
2024-01-12 21:10:02 +0100

    changes to switch files

--- bin/hm-switch.sh
+++ bin/hm-switch.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+set -e
 cd ~/.config/Mothership/config/
 NIXPKGS_ALLOW_INSECURE=1 home-manager switch --flake . --impure
 dotconfig.fish ${1:-base}
--- bin/nx-switch.sh
+++ bin/nx-switch.sh
@@ -1,2 +1,4 @@
+#!/usr/bin/env bash
+set -e
 cd ~/.config/Mothership/config
 sudo nixos-rebuild switch --flake .#desktop --impure
